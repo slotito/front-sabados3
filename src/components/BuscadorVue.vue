9058cbdee0607ba5f4f9de92587229eb15df58dd
Sergio L <selorigi@gmail.com>
2023-09-24 21:56:42 -0300

    Estetica buscador mejorada

--- src/components/BuscadorVue.vue
+++ src/components/BuscadorVue.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="buscador">
-        <h3>el buscador</h3>
+    
+    <input class="buscador-input" type="text" v-model="busqueda" @input="filtrarProductos" placeholder="Busque su producto" />
+    
 
     </div>
 
@@ -8,17 +10,59 @@
 </template>
 
 <script>    
+import axios from 'axios';
 
+export default {
+    name: 'BuscadorVue',
+  data() {
+    return {
+      busqueda: '', // Valor del campo de búsqueda
+        productos: [], // Array de productos
+
+    };
+  },
+  computed: {
+    productosFiltrados() {
+      // Filtrar productos en función de la búsqueda
+      return this.productos.filter(producto =>
+        producto.title.toLowerCase().includes(this.busqueda.toLowerCase())
+      );
+    },
+  },
+  methods: {
+    filtrarProductos() {
+      // Método para filtrar productos cuando se escribe en el campo de búsqueda
+    },
+  },
+    mounted() {
+        axios.get('https://fakestoreapi.com/products?limit=8')
+            .then(res => {
+                this.productos = res.data
+            })
+    },
+};
 </script>
 
 <style>
 
 .buscador {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     flex-grow: 100%;
     height: 15vh;
     padding: 6px;
     border-radius: 8px;
-    background-color: rgb(15, 133, 93);
+    background-color: rgb(140, 229, 201);
+    margin-top: 0.5rem;
+}
+
+.buscador-input {
+  width: 70%;
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 0.25rem;
 }
 
 </style>
